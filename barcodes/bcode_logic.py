39758897117e26a60b39163eb540ce9e344bf46d
jpirnay <jens@pirnay.com>
2023-04-17 22:15:26 +0200

    Multiple changes

--- barcodes/bcode_logic.py
+++ barcodes/bcode_logic.py
@@ -2,6 +2,7 @@
 This module contains the logic to create the different barcode elements
 """
 
+
 def create_barcode(
     kernel,
     channel,
@@ -12,16 +13,16 @@ def create_barcode(
     btype=None,
     code=None,
     notext=None,
-    asgroup=None,
 ):
     import barcode
-    from meerk40t.svgelements import Path, Matrix, Rect, Color
+    from meerk40t.svgelements import Color, Matrix, Path, Rect
 
     def poor_mans_svg_parser(svg_str, actionable):
         origin_x = float("inf")
         origin_y = float("inf")
         maximum_height = 0
         maximum_width = 0
+        data = []
         if actionable:
             # We run through it just to establish
             # the maximum_width and maximum_height
@@ -30,6 +31,7 @@ def create_barcode(
                 maximum_height,
                 origin_x,
                 origin_y,
+                dummy_data
             ) = poor_mans_svg_parser(svg_str, False)
             scale_x = 1
             scale_y = 1
@@ -44,7 +46,6 @@ def create_barcode(
         groupnode = None
         barcodepath = None
         pathcode = ""
-        data = []
         pattern_rect = "<rect"
         pattern_text = "<text"
         pattern_group_start = "<g "
@@ -100,43 +101,25 @@ def create_barcode(
                     )
                     this_wd = scale_x * elements.length_x(elem["width"])
                     this_ht = scale_y * elements.length_y(elem["height"])
-                    if aspath:
-                        # A direct creation of the path with it's native methods failed for me
-                        # so I circumvent this by preparing a valid SVG path definition and let
-                        # the path parse it - this works, so I assume a flaw in my coding or
-                        # in the underlying application of the matrix (which should be very simple
-                        # as I provide all coordinates in base units) - whatever this method
-                        # works, the other doesn't....
+                    # A direct creation of the path with it's native methods failed for me
+                    # so I circumvent this by preparing a valid SVG path definition and let
+                    # the path parse it - this works, so I assume a flaw in my coding or
+                    # in the underlying application of the matrix (which should be very simple
+                    # as I provide all coordinates in base units) - whatever this method
+                    # works, the other doesn't....
 
-                        # barcodepath.move(this_x, this_y)
-                        # barcodepath.line(this_x + this_wd, this_y)
-                        # barcodepath.line(this_x + this_wd, this_y + this_ht)
-                        # barcodepath.line(this_x, this_y + this_ht)
-                        # barcodepath.line(this_x, this_y)
-                        # barcodepath.closed(relative=True)
-                        pathcode += f"M {this_x:.1f} {this_y:.1f} "
-                        pathcode += f"L {this_x + this_wd:.1f} {this_y:.1f} "
-                        pathcode += f"L {this_x + this_wd:.1f} {this_y + this_ht:.1f} "
-                        pathcode += f"L {this_x:.1f} {this_y + this_ht:.1f} "
-                        pathcode += f"L {this_x:.1f} {this_y:.1f} "
-                        pathcode += f"z "
-                    else:
-                        rect = Rect(
-                            x=this_x,
-                            y=this_y,
-                            width=this_wd,
-                            height=this_ht,
-                        )
-                        node = elements.elem_branch.add(shape=rect, type="elem rect")
-                        node.stroke = (
-                            None if elem["stroke"] is None else Color(elem["stroke"])
-                        )
-                        node.fill = (
-                            None if elem["fill"] is None else Color(elem["fill"])
-                        )
-                        data.append(node)
-                        if groupnode is not None:
-                            groupnode.append_child(node)
+                    # barcodepath.move(this_x, this_y)
+                    # barcodepath.line(this_x + this_wd, this_y)
+                    # barcodepath.line(this_x + this_wd, this_y + this_ht)
+                    # barcodepath.line(this_x, this_y + this_ht)
+                    # barcodepath.line(this_x, this_y)
+                    # barcodepath.closed(relative=True)
+                    pathcode += f"M {this_x:.1f} {this_y:.1f} "
+                    pathcode += f"L {this_x + this_wd:.1f} {this_y:.1f} "
+                    pathcode += f"L {this_x + this_wd:.1f} {this_y + this_ht:.1f} "
+                    pathcode += f"L {this_x:.1f} {this_y + this_ht:.1f} "
+                    pathcode += f"L {this_x:.1f} {this_y:.1f} "
+                    pathcode += f"z "
                 else:
                     # just establish dimensions
                     # print (f"check rect extent for x={elem['x']}, y={elem['y']}, wd={elem['width']}, ht={elem['height']}")
@@ -254,7 +237,7 @@ def create_barcode(
                     # maximum_height = max(maximum_height, this_extent_y)
             elif pattern_group_end in line:
                 #  print (f"Group end: '{line}'")
-                if actionable and aspath:
+                if actionable:
                     # We need to close and add the path
                     barcodepath = Path(
                         fill=Color("black"),
@@ -287,8 +270,7 @@ def create_barcode(
             elif pattern_group_start in line:
                 # print(f"Group start: '{line}'")
                 if actionable:
-                    if aspath:
-                        pathcode = ""
+                    pathcode = ""
                     if not skiptext:
                         groupnode = elements.elem_branch.add(
                             type="group",
@@ -296,13 +278,11 @@ def create_barcode(
                             id=f"{btype}",
                         )
                         data.append(groupnode)
-        return maximum_width, maximum_height, origin_x, origin_y
+        return maximum_width, maximum_height, origin_x, origin_y, data
 
     # ---------------------------------
-
     _ = kernel.translation
     elements = kernel.elements
-    data = []
     orgcode = code
     if code is not None:
         code = elements.mywordlist.translate(code)
@@ -320,10 +300,7 @@ def create_barcode(
     except ValueError:
         channel(_("Invalid dimensions provided"))
         return None
-    aspath = True
     skiptext = False
-    if asgroup is not None:
-        aspath = False
     if notext is not None:
         skiptext = True
 
@@ -343,14 +320,98 @@ def create_barcode(
         my_barcode.build()
     bytes_result = my_barcode.render()
     result = bytes_result.decode("utf-8")
-    max_wd, max_ht, ox, oy = poor_mans_svg_parser(result, True)
+    max_wd, max_ht, ox, oy, data = poor_mans_svg_parser(result, True)
     for node in data:
         if node.type == "elem path":
             # We store the data for later customisation
             node.mktext = orgcode
             node.mkbarcode = "ean"
             node.mkparam = btype
+            node._translated_text = code
     return data
+
+def render_qr(context, version, errc, boxsize, border, wd, code):
+    import qrcode
+    import qrcode.image.svg
+    from meerk40t.core.units import Length
+    from meerk40t.svgelements import Matrix, Path
+
+    print(
+        f"Render called with: version={version}, errc={errc}, boxisze={boxsize}, border={border}"
+    )
+    path = None
+    elements = context.elements
+    qr = qrcode.QRCode(
+        version=version,
+        error_correction=errc,
+        box_size=boxsize,
+        border=border,
+    )
+
+    qr.add_data(code)
+    factory = qrcode.image.svg.SvgPathImage
+    qr.image_factory = factory
+    if version is None:
+        img = qr.make_image(fit=True)
+    else:
+        img = qr.make_image()
+    # We do get a ready to go svg string, but let's try to
+    # extract some basic information
+    # 1) Dimension
+
+    dim_x = "3cm"
+    dim_y = "3cm"
+    test = Length(wd)
+    print(dim_x, dim_y, wd, test.cm)
+    txt = str(img.to_string())
+    pattern = 'viewBox="'
+    idx = txt.find(pattern)
+    if idx >= 0:
+        txt = txt[idx + len(pattern) :]
+        idx = txt.find('"')
+        if idx >= 0:
+            txt = txt[:idx]
+            vp = txt.split(" ")
+            dim_x = str(float(vp[2]) - 2 * border) + "mm"
+            dim_y = str(float(vp[3]) - 2 * border) + "mm"
+    svg_x = elements.length_x(dim_x)
+    svg_y = elements.length_y(dim_y)
+    # 2) Path definition
+    txt = str(img.to_string())
+    pathstr = ""
+    pattern = '<path d="'
+    idx = txt.find(pattern)
+    if idx >= 0:
+        txt = txt[idx + len(pattern) :]
+        idx = txt.find(" id=")
+        if idx >= 0:
+            txt = txt[: idx - 1]
+            pathstr = txt
+    if len(pathstr):
+        mm = elements.length("1mm")
+        sx = mm
+        sy = mm
+
+        sx *= wd / svg_x
+        sy *= wd / svg_y
+        # px = -border * mm + xp
+        # py = -border * mm + yp
+        matrix = Matrix(f"scale({sx},{sy})")
+        # channel(f"scale({sx},{sy})")
+        path = Path(
+            fill="black",
+            stroke=None,
+            width=dim_x,
+            height=dim_y,
+            matrix=Matrix(),
+        )
+        path.parse(pathstr)
+        matrix = Matrix(f"scale({sx},{sy})")
+        path.transform *= matrix
+        # channel(f"pathstr={pathstr[:10]}...{pathstr[-10:]}")
+        # channel(f"x={dim_x}, y={dim_y},")
+    return path
+
 
 def create_qr(
     kernel,
@@ -367,8 +428,7 @@ def create_qr(
     **kwargs,
 ):
     import qrcode
-    import qrcode.image.svg
-    from meerk40t.svgelements import Path, Matrix
+
     _ = kernel.translation
     elements = kernel.elements
     data = []
@@ -402,100 +462,100 @@ def create_qr(
     except ValueError:
         channel(_("Invalid dimensions provided"))
         return None
-
-    qr = qrcode.QRCode(
-        version=version,
-        error_correction=errc,
-        box_size=boxsize,
-        border=border,
+    path = render_qr(kernel, version, errc, boxsize, border, wd, code)
+    if path is None:
+        return None
+    node = elements.elem_branch.add(
+        path=abs(path),
+        stroke_width=0,
+        stroke_scaled=False,
+        type="elem path",
+        fillrule=0,  # nonzero
+        label=f"qr={code}",
     )
-
-    qr.add_data(code)
-    factory = qrcode.image.svg.SvgPathImage
-    qr.image_factory = factory
-    if version is None:
-        img = qr.make_image(fit=True)
-    else:
-        img = qr.make_image()
-    # We do get a ready to go svg string, but let's try to
-    # extract some basic information
-    # 1) Dimension
-    dim_x = "3cm"
-    dim_y = "3cm"
-    txt = str(img.to_string())
-    pattern = 'viewBox="'
-    idx = txt.find(pattern)
-    if idx >= 0:
-        txt = txt[idx + len(pattern) :]
-        idx = txt.find('"')
-        if idx >= 0:
-            txt = txt[:idx]
-            vp = txt.split(" ")
-            dim_x = str(float(vp[2]) - 2 * border) + "mm"
-            dim_y = str(float(vp[3]) - 2 * border) + "mm"
-    svg_x = elements.length_x(dim_x)
-    svg_y = elements.length_y(dim_y)
-    # 2) Path definition
-    txt = str(img.to_string())
-    pathstr = ""
-    pattern = '<path d="'
-    idx = txt.find(pattern)
-    if idx >= 0:
-        txt = txt[idx + len(pattern) :]
-        idx = txt.find(" id=")
-        if idx >= 0:
-            txt = txt[: idx - 1]
-            pathstr = txt
-    if len(pathstr):
-        mm = elements.length("1mm")
-        sx = mm
-        sy = mm
-
-        sx *= wd / svg_x
-        sy *= wd / svg_y
-        px = -border * mm + xp
-        py = -border * mm + yp
-        matrix = Matrix(f"scale({sx},{sy})")
-        # channel(f"scale({sx},{sy})")
-        path = Path(
-            fill="black",
-            stroke=None,
-            width=dim_x,
-            height=dim_y,
-            matrix=Matrix(),
-        )
-        path.parse(pathstr)
-        matrix = Matrix(f"scale({sx},{sy})")
-        path.transform *= matrix
-        # channel(f"pathstr={pathstr[:10]}...{pathstr[-10:]}")
-        # channel(f"x={dim_x}, y={dim_y},")
-        node = elements.elem_branch.add(
-            path=abs(path),
-            stroke_width=0,
-            stroke_scaled=False,
-            type="elem path",
-            fillrule=0,  # nonzero
-            label=f"qr={code}",
-        )
-        node.matrix.post_translate(-node.bounds[0] + xp, -node.bounds[1] + yp)
-        node.modified()
-        node.mktext = orgcode
-        node.mkbarcode = "qr"
-        node.mkparam = (version, errc, boxsize, border)
-        # elements.set_emphasis([node])
-        # node.focus()
+    node.matrix.post_translate(-node.bounds[0] + xp, -node.bounds[1] + yp)
+    node.modified()
+    node.mktext = orgcode
+    node._translated_text = code
+    node.mkbarcode = "qr"
+    node.mkparam = (version, errc, boxsize, border)
 
     data = [node]
     return data
 
-def update_ean(context, node, newtext):
+
+def update_ean(context, node, code):
     if node is None:
         return
-    if not hasattr(node, "mktext") or not hasattr(node, "mkbarcode") or getattr(node, "mkbarcode") != "ean":
+    if (
+        not hasattr(node, "mktext")
+        or not hasattr(node, "mkbarcode")
+        or getattr(node, "mkbarcode") != "ean"
+    ):
         return
 
-def update_qr(context, node, newtext):
+
+def update_qr(context, node, code):
+    import qrcode
+
     if node is None:
         return
-    if not hasattr(node, "mktext") or not hasattr(node, "mkbarcode") or getattr(node, "mkbarcode") != "qr":
+    if (
+        not hasattr(node, "mktext")
+        or not hasattr(node, "mkbarcode")
+        or getattr(node, "mkbarcode") != "qr"
+    ):
         return
+    print(f"update called with {code}")
+    elements = context.elements
+    orgcode = code
+    if code is not None:
+        code = elements.mywordlist.translate(code)
+
+    version = None
+    errcode = "M"
+    if errcode == "L":
+        errc = qrcode.constants.ERROR_CORRECT_L
+    elif errcode == "Q":
+        errc = qrcode.constants.ERROR_CORRECT_Q
+    elif errcode == "H":
+        errc = qrcode.constants.ERROR_CORRECT_H
+    else:
+        errc = qrcode.constants.ERROR_CORRECT_M
+
+    boxsize = 10
+    border = 4
+    bb = node.bounds
+    wd = node.bounds[2] - node.bounds[0]
+    print(bb, wd)
+    if hasattr(node, "mkparam"):
+        valu = node.mkparam
+        if isinstance(valu, (list, tuple)) and len(valu) > 3:
+            version = valu[0]
+            errc = valu[1]
+            boxsize = valu[2]
+            border = valu[3]
+
+    path = render_qr(context, version, errc, boxsize, border, wd, code)
+
+    olda = node.path.transform.a
+    oldb = node.path.transform.b
+    oldc = node.path.transform.c
+    oldd = node.path.transform.d
+    olde = node.path.transform.e
+    oldf = node.path.transform.f
+    node.path = path
+    node.path.transform.a = olda
+    node.path.transform.b = oldb
+    node.path.transform.c = oldc
+    node.path.transform.d = oldd
+    node.path.transform.e = olde
+    node.path.transform.f = oldf
+    # print (f"x={node.mkcoordx}, y={node.mkcoordy}")
+    # node.path.transform = Matrix.translate(node.mkcoordx, node.mkcoordy)
+    oldtext = node.mktext
+    old_trans = getattr(node, "_translated_text", "")
+    print(f"Updated: from {oldtext} ({old_trans}) -> {orgcode} ({code})")
+    node.mktext = orgcode
+    node._translated_text = code
+    node.altered()
