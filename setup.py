cde3449b2e872568badfe89678952aa1e97e3e1e
jpirnay <jens@pirnay.com>
2023-07-13 13:10:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,4 @@ setup(
         "qrcode>=7.0",
         "python-barcode>=0.10",
     ],
-  name = 'meerk40t-barcodes',         # How you named your package folder 
-  packages = ['meerk40t-barcodes'],   # Chose the same as "name"
-  version = '0.2',
-  license='MIT',  
-  description = 'This plugin for meerk40t provides barcode creation functionality',
-  author = 'jpirnay',                   # Type in your name
-  author_email = 'your.email@domain.com',     
-  url = 'https://github.com/meerk40t/meerk40t-barcodes',
-  download_url = 'https://github.com/meerk40t/meerk40t-barcodes/archive/v02.tar.gz',    
-  keywords = ['meerk40t', 'barcodes', 'plugin'],
 )
